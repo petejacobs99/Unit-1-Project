5fe3220f5b670fd225e82fb556dc679d585a9014
petejacobs99 <petejacobs99@gmail.com>
2023-05-03 20:14:33 -0400

    Major updates to spin and wager functionality

--- app.js
+++ app.js
@@ -12,46 +12,80 @@ const slot2Image = document.getElementById('slot2Image')
 const slot3Image = document.getElementById('slot3Image')
 const spinButton = document.getElementById('spinButton')
 const alertMessage = document.getElementById('alertMessage')
-const walletText = document.getElementById('wallet')
+const minusOneButton = document.getElementById('minusOneButton')
+const minusFiveButton = document.getElementById('minusFiveButton')
+const plusOneButton = document.getElementById('plusOneButton')
+const plusFiveButton = document.getElementById('plusFiveButton')
+const wagerAmount = document.getElementById('wagerAmount')
+const walletAmount = document.getElementById('walletAmount')
 
 
 
 	/*----- event listeners -----*/
 spinButton.addEventListener('click', spin)
+minusOneButton.addEventListener('click', function(){changeWager(-1)})
+minusFiveButton.addEventListener('click', function(){changeWager(-5)})
+plusOneButton.addEventListener('click', function(){changeWager(1)})
+plusFiveButton.addEventListener('click', function() {changeWager(5)})
+window.addEventListener('load', init);
+
 
 	/*----- functions -----*/
+function init(){
+    wallet = 100
+    walletAmount.innerHTML = wallet
+    wager = 10
+    wagerAmount.innerHTML = wager
+}
 
 function spin(){
-    slot1Image.src = slotItemSources[Math.floor(Math.random() * 3)]
-    slot2Image.src = slotItemSources[Math.floor(Math.random() * 3)]
-    slot3Image.src = slotItemSources[Math.floor(Math.random() * 3)]
+    if((wallet - wager) >= 0){
+        wallet = wallet - wager
+        walletAmount.innerHTML = wallet
+        slot1Image.src = slotItemSources[Math.floor(Math.random() * 3)]
+        slot2Image.src = slotItemSources[Math.floor(Math.random() * 3)]
+        slot3Image.src = slotItemSources[Math.floor(Math.random() * 3)]
+    } else {
+        alert("Not enough money!")
+    }
     checkForWin()
 }
 
-function init(){
- wallet = 100
-
- wager = 10
-}
-
 function checkForWin(){
-
     if(slot1Image.src === slot2Image.src && slot2Image.src === slot3Image.src){
         win = true
+        payoutWinnings()
     } else {
         win = false
-    }  
-    checkWager()
-}
-
-function checkWager(){
-    if(win === true){
-
-    } else if (win === false){
-
-    } else {
-        alert('error')
     }
+
 }
 
-document.addEventListener('DOMContentLoaded', init)
+function payoutWinnings(){
+console.log(slot1Image.src)
+    if(slot1Image.src === "file:///Users/peterjacobs/portfolio-projects/projects/unit-1-project/assets/ketchup.jpeg"){
+        wallet = wallet + (wager * 2)
+        walletAmount.innerText = wallet
+        alert(`YOU WIN! PAYING OUT: ${wager * 2}`)
+    } else if (slot1Image.src === "file:///Users/peterjacobs/portfolio-projects/projects/unit-1-project/assets/fries.jpeg"){
+        wallet = wallet + (wager * 3)
+        walletAmount.innerText = wallet
+        alert(`YOU WIN! PAYING OUT: ${wager * 3}`)
+    } else if (slot1Image.src === "file:///Users/peterjacobs/portfolio-projects/projects/unit-1-project/assets/burger.jpeg"){
+        wallet = wallet + (wager * 4)
+        walletAmount.innerText = wallet
+        alert(`YOU WIN! PAYING OUT: ${wager * 4}`)
+    } else {
+        alert("YOU WIN BUT WE ENCOUNTERED AN ERROR")
+    }
+
+}
+
+function changeWager(amountOfChange){
+    if((wager + amountOfChange) >= 1){
+    wager = wager + amountOfChange
+    wagerAmount.innerText = wager
+} else {
+    alert('Wager cannot be less than $1')
+}
+}
